9e359a823c504bbd1725f5a0cf34e97480f83766
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2024-01-03 13:22:45 +0000

    - Added todo.md - Made buttons - Made string literal alerts and onclick events - Formatted page with question, then 2 answer buttons

--- src/App.js
+++ src/App.js
@@ -1,22 +1,95 @@
 import logo from './logo.svg';
 import './App.css';
 
+//Variables
+let questionIndex = 0;
+
+function Button1_1() {
+  questionIndex++;
+
+  return (
+    <button onClick={() => alert(`Button1_1\nQuestion Index: ${questionIndex}`)}>Button1_1</button>
+  )
+}
+
+function Button1_2() {
+  questionIndex++;
+  
+  return (
+    <button onClick={() => alert(`Button1_2\nQuestion Index: ${questionIndex}`)}>Button1_2</button>
+  )
+}
+
+function Button2_1() {
+  questionIndex++;
+  
+  return (
+    <button onClick={() => alert(`Button2_1\nQuestion Index: ${questionIndex}`)}>Button2_1</button>
+  )
+}
+
+function Button2_2() {
+  questionIndex++;
+  
+  return (
+    <button onClick={() => alert(`Button2_2\nQuestion Index: ${questionIndex}`)}>Button2_2</button>
+  )
+}
+
+function CurrentButtons({questionIndex}) {
+  switch (questionIndex) {
+  
+    case 0: 
+      {
+        return (
+          <div>
+            <p>
+              Here is question 1 ahhhh!
+            </p>
+            <Button1_1 />
+            <Button1_2 />
+          </div>
+        )
+        break;
+      }
+
+    case 1:
+      {
+        return (
+          <div>
+            <p>
+              Here HAYs QEUBSIN 2 ahhhh!
+            </p>
+            <Button1_1 />
+            <Button1_2 />
+          </div>
+        )
+        break;
+      }
+  }
+}
+
+//function that returns the questionindex of APp(): 
+// function QuestionIndex() {
+//   return (
+//     <p>
+//       {questionIndex}
+//     </p>
+//   )
+// }
+
+
 function App() {
   return (
+    //how to comment inside div: 
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hola mundo
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CurrentButtons questionIndex={questionIndex} />
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
       </header>
     </div>
   );
---
+++ todo.md
@@ -0,0 +1,7 @@
+# Buzzfeed2 To-do list
+- [x] Project boots as website
+- [ ] Add basic features:
+- [ ] Header
+- [ ] Footer
+- [x] Main Content: 2 clickable buttons under text
+- [ ] Edit bootstrap to be good colours n such
